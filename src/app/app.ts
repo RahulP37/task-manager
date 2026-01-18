4f987b7ee34d9e6b3bc3b0f85ca823bec5743fcf
Rahul Patil <rp9460121@gmail.com>
2026-01-18 17:58:38 +0530

    Initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,20 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { headercomponent } from "./header/header.componet";
+import { User } from "./user/user";
+import { DUMMY_USERS } from './dummy-users';
+ 
+
+
+@Component({
+  selector: 'app-root',
+  imports: [headercomponent, User],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+  
+})
+export class App {
+  protected readonly title = signal('my-first-app');
+  users=DUMMY_USERS;
+  
+}
---
+++ src/app/user/user.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+ 
+@Component({
+  selector: 'app-user',
+  standalone:true,
+  imports: [],
+  templateUrl: './user.html',
+  styleUrl: './user.css',
+})
+export class User {
+ 
+
+ @Input() avatar!:string;
+ @Input() name!:string;
+
+  get imagePath(){
+    return 'assets/users/'+this.avatar;
+
+  }
+
+ onSelectuser(){
+  
+ 
+ }
+}
