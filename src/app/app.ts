ce0b1e52523ad271e5259ff75e31fd04873e27eb
Rahul Patil <rp9460121@gmail.com>
2026-01-19 18:51:42 +0530

    displaying the names when the person is clicked

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,23 +3,31 @@ import { RouterOutlet } from '@angular/router';
 import { headercomponent } from "./header/header.componet";
 import { User } from "./user/user";
 import { DUMMY_USERS } from './dummy-users';
- 
+import { Tasks } from './tasks/tasks';
 
 
 @Component({
   selector: 'app-root',
-  imports: [headercomponent, User],
+  imports: [headercomponent, User, Tasks],
   templateUrl: './app.html',
   styleUrl: './app.css',
-  
+
 })
 export class App {
   protected readonly title = signal('my-first-app');
-  users=DUMMY_USERS;
-  
+  users = DUMMY_USERS;
+  name_used_in_task!: string;
 
-  onselectuser(ids:string){
-    console.log('selected user with id',ids);
 
+  onselectuser(ids: string) {
+    for (const key of this.users) {
+      if (key.id === ids) {
+        this.name_used_in_task = key.name;
+        break;
+      }
+
+    }
+
+    console.log('selected user with id', ids);
   }
 }
---
+++ src/app/tasks/tasks.ts
@@ -0,0 +1,15 @@
+import { Component, computed, Input } from '@angular/core';
+ 
+import { DUMMY_USERS } from '../dummy-users';
+@Component({
+  selector: 'app-tasks',
+  imports: [ ],
+  templateUrl: './tasks.html',
+  styleUrl: './tasks.css',
+})
+export class Tasks {
+  @Input({required:true}) name!:string;
+   
+  
+
+}
